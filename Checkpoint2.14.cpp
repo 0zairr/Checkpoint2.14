16465eaa0712b661451420f561fa3bc1607114f1
ozair <ozair@OZAIR>
2024-09-24 23:16:52 -0400

    Add project files.

---
+++ Checkpoint2.14.cpp
@@ -0,0 +1,26 @@
+/* Program File Name: Checkpoint2.14
+   Programmer: Ozair Ghaissi
+   Date: September 2024
+   Requirements: 
+   Write a program that has the following character variables: 
+   first, middle, and last. Store your initials in these variables 
+   then disply them on the screen.
+*/
+
+#include <iostream>
+
+int main()
+{
+    char o;
+    char n;
+    char g;
+    std::cout << "o:";
+    std::cin >> o;
+    std::cout << "n:";
+    std::cin >> n;
+    std::cout << "g:";
+    std::cin >> g;
+    std::cout << "ong:" << o << n << g;
+
+
+}
